75af59b6b5347ff4992a4857fbc808b78065d2dd
Sarah Schneider <143359626+ses011@users.noreply.github.com>
2025-04-20 21:20:55 -0400

    Initialize flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# kakurasu_puzzle_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/kakurasu_puzzle_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kakurasu_puzzle_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
